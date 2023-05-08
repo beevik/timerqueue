05e4fe795e6c610a66d7e4ec6c177d75a909a296
Brett Vickers <brett@beevik.com>
2023-05-07 22:13:58 -0700

    Update unit tests

--- queue_test.go
+++ queue_test.go
@@ -19,13 +19,19 @@ func (o *object) OnTimer(t time.Time) {
 func populateQueue(t *testing.T, now time.Time) *Queue {
 	q := New()
 
-	count := 200
+	count := 300
 	objects := make([]*object, count)
 
 	// Add a bunch of objects to the queue in random order.
 	for i, j := range rand.Perm(count) {
 		tm := now.Add(time.Duration(i+1) * time.Hour)
 		objects[j] = &object{j}
+		q.Schedule(objects[j], tm)
+	}
+
+	// Reschedule all the objects in a different random order.
+	for i, j := range rand.Perm(count) {
+		tm := now.Add(time.Duration(i+1) * time.Hour)
 		q.Schedule(objects[j], tm)
 	}
 
@@ -36,6 +42,20 @@ func populateQueue(t *testing.T, now time.Time) *Queue {
 	return q
 }
 
+func TestEmptyQueue(t *testing.T) {
+	queue := New()
+
+	o, _ := queue.PeekFirst()
+	if o != nil {
+		t.Error("Expected nil peek")
+	}
+
+	o, _ = queue.PopFirst()
+	if o != nil {
+		t.Error("Expected nil pop")
+	}
+}
+
 func TestQueue(t *testing.T) {
 	for iter := 0; iter < 100; iter++ {
 		now := time.Date(2015, 1, 1, 0, 0, 0, 0, time.UTC)
@@ -43,7 +63,11 @@ func TestQueue(t *testing.T) {
 
 		// Make sure objects are removed from the queue in order.
 		for prev := now; queue.Len() > 0; {
+			_, ptm := queue.PeekFirst()
 			_, tm := queue.PopFirst()
+			if tm != ptm {
+				t.Errorf("Peek/Pop mismatch.\n")
+			}
 			if tm.Sub(prev) != time.Hour {
 				t.Errorf("Invalid queue ordering.\n"+
 					"     Got: %v\n"+
